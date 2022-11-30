2abdb5ccd70562c1810faabc082c3cda940ef341
Justin Dionisio <justindionisio21@yahoo.com>
2022-11-30 14:31:39 -0800

    Added escape event for modal

--- script.js
+++ script.js
@@ -22,3 +22,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+	// if pressedd key is escape and modal does not contain hidden class then invoke 'closeModal()'
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal();
+	}
+});
